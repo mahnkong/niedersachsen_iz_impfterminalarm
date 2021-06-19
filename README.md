5fec5f8a99d568ac6a060d6e5d0aa8b9a50ea95a
Andreas Mahnke <mahnkong@gmx.de>
2021-06-19 16:56:47 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Terminalarm für Corona - Impfzentrum in Niedersachen
+
+Dieses Git-Repo enthält ein kleines Shell - Script, welches den Nutzer unter Angabe seiner PLZ über freie Termine im entsprechenden Corona - Impfzentrum in Niedersachsen benachrichtigt.
+Ich habe dieses Script für micht selber gebaut um schnell eine Benachrichtigung zu bekommen und mir so einen Termin schnappen können. Daher veröffentliche ich es hier um auch anderen Personen zu helfen einen Termin zu ergattern. (andere Suchdienste waren teilweise zu langsam, in meinem LK waren Termine nur gut 30 Sekunden verfügbar)
+
+Benachrichtigungen erfolgen via Terminalausgabe und optional auch via Audio (.wav Files via aplay) oder E-Mail (GMail).
+
+# Benötigte Softwarepakete
+
+**Da ich das Script nur unter Ubuntu bzw. Debian (Raspbian auf RaspberryPi) genutzt habe, kann ich die Pakete nur für diese Systeme benennen)**
+
+Um das Script nutzen zu können, müssen folgende Pakete installiert sein.
+
+- jq
+- curl
+
+Für das Abspielen der Sounddatei:
+
+- alsa-utils
+
+Für den Mailversand:
+
+- sendemail
+- libio-socket-ssl-perl
+- libnet-ssleay-perl
+
+# Verwendung
+
+Das Script kann einfach in der Kommandozeile gestartet werden und läuft bis es abgebrochen wird:
+
+```
+bash ./niedersachsen_iz_impfterminalarm.sh -z ZIPCODE [ -s SOUND_FILE -r MAIL_RCPT -f GMAIL_MAILFROM -p GMAIL_SMTPAUTH_PASSWORD ]
+```
+
+Zum Starten im Hintergrund kann z.B. das Kommando *nohup* genutzt werden.
+
+## Parameter
+
+- -z: Postleitzahl des Nutzers (muss immer angegeben werden, das zuständige Impfzentrum wird damit ermittelt)
+- -s: Sounddatei, wird abgespielt falls freie Termine gefunden werden
+- -r: E-Mail Adresse, an die Benachrichtigungen über freie Termine versandt werden
+- -f: Absendeadresse für die E-Mail Benachrichtigungen (wird auch für Anmeldung an GMail genutzt)
+- -p: GMail Passwort für den Versand der E-Mail (Empfehle Verwendung von App - Password, kann im Google - Konto erzeugt werden)
---
+++ niedersachsen_iz_impfterminalarm.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+function usage() {
+  echo "Usage: bash $0 -z ZIPCODE [ -s SOUND_FILE -r MAIL_RCPT -f GMAIL_MAILFROM -p GMAIL_SMTPAUTH_PASSWORD ]"
+}
+
+show_usage=false
+while getopts ":r:z:s:f:p:h" opt; do
+  case ${opt} in
+    z) zipcode="$OPTARG"
+    ;;
+    s) sound_file="$OPTARG"
+    ;;
+    r) mail_rcpt="$OPTARG"
+    ;;
+    f) gmail_mail_from="$OPTARG"
+    ;;
+    p) gmail_smtpauth_password="$OPTARG"
+    ;;
+    h) usage
+       exit
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+gmail_mail_from=${gmail_mail_from:-}
+gmail_smtpauth_password=${gmail_smtpauth_password:-}
+mail_rcpt=${mail_rcpt:-}
+sound_file=${sound_file:-}
+zipcode=${zipcode:-}
+
+send_email=false
+play_sound=false
+
+## Check vars
+if [[ "${zipcode}" = "" ]]; then
+  echo "Zipcode not specified!" >&2
+  usage
+  exit 1
+fi
+
+for required_command in curl jq; do
+  if ! which ${required_command} >/dev/null 2>&1; then
+    echo "Required command '${required_command}' not found!" >&1
+    exit 1
+  fi
+done
+
+
+if [[ "${mail_rcpt}" != "" && "${gmail_mail_from}" != "" &&  "${gmail_smtpauth_password}" != "" ]]; then
+  if ! which sendemail > /dev/null 2>&1; then
+    echo "Command 'sendemail' not in path, will not send emails!" >&1
+  else
+    send_email=true
+  fi
+else
+  echo "Required parameters for sending emails not set, will not send emails!" >&1
+fi
+
+if [[ "${sound_file}" != "" && -f "${sound_file}" ]]; then
+  if ! which aplay > /dev/null 2>&1; then
+    echo "Command 'aplay' not in path, will not play sound!" >&1
+  else
+    play_sound=true
+  fi
+else
+  echo "Sound file not specified or does not exist, will not play sound!" >&1
+fi
+
+
+mail_subject=Impfterminalarm
+slots_free_msg_template="Termin(e) verfuegbar (Anzahl: %s) fuer PLZ: %s, hier geht es zur Anmeldung -> https://www.impfportal-niedersachsen.de/portal/#/appointment/public"
+alarm_already_notified=false
+
+tmpfile=$(mktemp)
+
+while true; do
+  current_msg="out of stock"
+  curl -s https://www.impfportal-niedersachsen.de/portal/rest/appointments/findVaccinationCenterListFree/${zipcode}\?stiko\=\&count\=1 > ${tmpfile}
+  if cat ${tmpfile} | jq '.resultList[].outOfStock' | grep -qv "true"; then
+    freeSlots=$(cat ${tmpfile} | jq '.resultList[].freeSlotSizeOnline')
+    aplay "$sound_file" > /dev/null 2>&1
+    current_msg=$(printf "$slots_free_msg_template" ${freeSlots} ${zipcode})
+
+    if ! ${alarm_already_notified}; then
+      if ${send_email}; then
+        sendemail -f "${gmail_mail_from}" -u "${mail_subject}" -t "${mail_rcpt}" -s  "smtp.gmail.com:587" -o tls=yes -xu "${gmail_mail_from}" -xp "${gmail_smtpauth_password}" -m "$current_msg"
+        alarm_already_notified=true
+      fi
+    fi
+  else
+    alarm_already_notified=false
+  fi
+  echo "$(date) :: $current_msg"
+  sleep 1
+done
+
